27697f2dc90bd70db556566327e87735ed13aa9a
kocmax95 <maksymiliankoczaski@gmail.com>
2019-07-20 14:44:00 +0200

    dzielenie na pliki

---
+++ src/app/actors/duck/actions.js
@@ -0,0 +1,15 @@
+import types from './types'
+
+
+const addActor = item => ({
+    type: types.'ADD_ACTOR', item
+});
+
+const resetActors = item => ({
+    type: types.'RESET_ACTORS', item
+});
+
+export default {
+    add,
+    reset
+}
